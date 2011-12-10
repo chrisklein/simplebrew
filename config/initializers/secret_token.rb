f369683a32541a8af31887737a5ff037bf5b04ea
Christopher Klein <chrisklein303@gmail.com>
2011-12-10 13:42:27 -0700

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Simplebrew::Application.config.secret_token = 'bd0db7b7401428fe9d9b8fcb9b1310c9145114494382cb6726c34f2d187f86dcba3a2d992ad45c626034b95a44a542a2ff3e8dc92c2a388185c152061e76786d'
