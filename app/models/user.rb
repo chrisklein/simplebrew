040c339c50089a05fa2ebc981dc6320f361ec0d1
Christopher Klein <chrisklein303@gmail.com>
2011-12-11 18:43:01 -0700

    continuing to build out user model and show page.

--- app/models/user.rb
+++ app/models/user.rb
@@ -10,6 +10,7 @@
 #
 
 class User < ActiveRecord::Base
+  attr_accessor   :password #attr_accessor only applies to models in memory.
   attr_accessible :name, :email, :password, :password_confirmation
   
   email_regex = /[\w+\-.]+@[a-z\d\-.]+\.[a-z]+/i
@@ -23,6 +24,38 @@ class User < ActiveRecord::Base
                        
   validates :password, :presence     => true,
                        :confirmation => true,
-                       :length       => { :within => 6..40 }                                    
+                       :length       => { :within => 6..40 }   
+                       
+  before_save :encrypt_password  
+  
+  # Return true if the user's password matches the submitted password.
+  def has_password?(submitted_password)
+    encrypted_password == encrypt(submitted_password)
+  end   
+  
+  def self.authenticate(email, submitted_password)
+    user = find_by_email(email)
+    return nil  if user.nil?
+    return user if user.has_password?(submitted_password)
+  end
+  
+  private
+
+    def encrypt_password
+      self.salt = make_salt unless has_password?(password)
+      self.encrypted_password = encrypt(self.password)
+    end
+
+    def encrypt(string)
+      secure_hash("#{salt}--#{string}")
+    end
+
+    def make_salt
+      secure_hash("#{Time.now.utc}--#{self.password}")
+    end
+
+    def secure_hash(string)
+      Digest::SHA2.hexdigest(string)
+    end                                                
                        
 end
