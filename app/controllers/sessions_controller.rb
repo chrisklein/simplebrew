085a3cc017d7efb927a3ce75e1913f678a26d26b
Christopher Klein <chrisklein303@gmail.com>
2011-12-22 11:43:52 -0700

    sign-in page in place

---
+++ app/controllers/sessions_controller.rb
@@ -0,0 +1,13 @@
+class SessionsController < ApplicationController
+  
+  def new
+    @title = "Sign in"
+  end
+
+  def create
+  end
+
+  def destroy
+  end
+
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,14 @@
 Simplebrew::Application.routes.draw do
   
   resources :users
+  resources :sessions, :only => [:new, :create, :destroy]
 
   root :to=> "pages#home"
   
   match '/contact',             :to => 'pages#contact'
   match '/signup',              :to => 'users#new'
+  match '/signin',              :to => 'sessions#new'
+    match '/signout',           :to => 'sessions#destroy'
   
   # The priority is based upon order of creation:
   # first created -> highest priority.
