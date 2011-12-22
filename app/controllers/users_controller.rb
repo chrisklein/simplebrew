6477915578dbfd8efa7427c7b947a0300c5545b8
Christopher Klein <chrisklein303@gmail.com>
2011-12-22 16:00:53 -0700

    added authentication for user settings and styled nav

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,6 @@
 class UsersController < ApplicationController
+  before_filter :authenticate, :only => [:edit, :update]
+  before_filter :correct_user, :only => [:edit, :update]
   
   def show
     @user = User.find(params[:id])
@@ -23,7 +25,7 @@ class UsersController < ApplicationController
   end
   
   def edit
-    @user = User.find(params[:id])
+    # No @user assignment because it's created in the correct_user before filter.
     @title = "Edit user"
   end
   
@@ -37,5 +39,16 @@ class UsersController < ApplicationController
       render 'edit'
     end
   end
+  
+  private
+
+    def authenticate
+      deny_access unless signed_in?
+    end
+    
+    def correct_user
+      @user = User.find(params[:id])
+      redirect_to(root_path) unless current_user?(@user)
+    end
 
 end
